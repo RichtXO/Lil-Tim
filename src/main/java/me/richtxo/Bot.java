901874b2839b166b84be1fe5ee27c8b3c7688f00
Richard Tsai <RichtXO@gmail.com>
2021-03-19 00:54:03 -0400

    adding stuff

--- src/main/java/me/richtxo/Bot.java
+++ src/main/java/me/richtxo/Bot.java
@@ -1,5 +1,7 @@
 package me.richtxo;
 
+import me.richtxo.commands.*;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -10,27 +12,26 @@ import javax.security.auth.login.LoginException;
 import java.util.EnumSet;
 
 public class Bot {
-    private Bot() throws LoginException {
+    public static void main(String[] args) throws LoginException {
         EventWaiter waiter = new EventWaiter();
+
+        CommandClientBuilder client = new CommandClientBuilder();
+        client.setPrefix(System.getenv("PREFIX"));
+
 
         DefaultShardManagerBuilder.createDefault(System.getenv("TOKEN"),
                 GatewayIntent.GUILD_MEMBERS,
                 GatewayIntent.GUILD_MESSAGES,
                 GatewayIntent.GUILD_VOICE_STATES
         )
-                .disableCache(EnumSet.of(
-                        CacheFlag.ACTIVITY,
-                        CacheFlag.EMOTE
-                ))
-                .enableCache(CacheFlag.VOICE_STATE)
-                .addEventListeners(waiter, new Listener())
-                .setShardsTotal(2)
-                .setActivity(Activity.listening("RichtXO"))
-                .build();
-    }
-
-    public static void main(String[] args) throws LoginException {
-        new Bot();
-        System.getenv();
+        .disableCache(EnumSet.of(
+                CacheFlag.ACTIVITY,
+                CacheFlag.EMOTE
+        ))
+        .enableCache(CacheFlag.VOICE_STATE)
+        .addEventListeners(waiter, client.build())
+        .setShardsTotal(4)
+        .setActivity(Activity.listening("RichtXO"))
+        .build();
     }
 }

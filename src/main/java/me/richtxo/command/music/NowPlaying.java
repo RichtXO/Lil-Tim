d58af8ffc1a5a65aedf05b0307b02cf214910adf
Richard Tsai <RichtXO@gmail.com>
2021-03-23 11:17:28 -0400

    fixed nowplaying time

--- src/main/java/me/richtxo/command/music/NowPlaying.java
+++ src/main/java/me/richtxo/command/music/NowPlaying.java
@@ -50,7 +50,7 @@ public class NowPlaying extends Command {
 
     private String formatTime(long timeInMillis) {
         final long hours = timeInMillis / TimeUnit.HOURS.toMillis(1);
-        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1) - hours * 60;
+        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1) - TimeUnit.HOURS.toMinutes(hours);
         final long seconds = timeInMillis % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
